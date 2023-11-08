e4314965060fd3aa7cf2d5f50383d0800c5bca11
Lisa Bekdache <lisa.bekdache@shadow.tech>
2023-11-08 15:30:15 +0100

    Workaround for proxmox 6.2, fix error 400 node duplicate argument

--- contrib/proxmox_pack_generator.py
+++ contrib/proxmox_pack_generator.py
@@ -215,7 +215,8 @@ class {{ class_name }}Action(ProxmoxAction):
             else:
                 if api_arg[2] == "boolean":
                     api_arg[1] = int(api_arg[1])
-                proxmox_kwargs[api_arg[0]] = api_arg[1]
+                if api_arg[0] != "node":
+                    proxmox_kwargs[api_arg[0]] = api_arg[1]
 
         return self.proxmox.{{ http_verb.lower() }}(
             f"{{ path.lstrip("/") }}",
